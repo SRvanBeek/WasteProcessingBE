a2f0e7079126e7d6d961a68e7c5806b2cc449d32
blaFish3 <30380030+blaFish3@users.noreply.github.com>
2022-10-12 15:15:21 +0200

    Spring initialize

---
+++ src/main/java/nl/groep14/ipsen2BE/Ipsen2BeApplication.java
@@ -0,0 +1,13 @@
+package nl.groep14.ipsen2BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipsen2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipsen2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/groep14/ipsen2BE/Ipsen2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.groep14.ipsen2BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipsen2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
