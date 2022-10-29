4c83801c52b464cdfda6e009e164d46f7a24e7f9
Roy van Delft <s1121703@student.hsleiden.nl>
2022-10-29 23:10:30 +0200

    FEAT: Added first security setup, and made the default Admin user with the according rights

---
+++ src/main/java/nl/groep14/ipsen2BE/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package nl.groep14.ipsen2BE.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("Admin")
+                        .password("{noop}admin123")
+                        .roles("ADMIN")
+                        .authorities("read", "write", "update", "delete")
+                        .build()
+        );
+
+    }
+
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests( auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
+}
