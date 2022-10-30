2543b16692d08dff420de13903605e703d59758a
SRvanBeek <0705stijncb@gmail.com>
2022-10-30 14:11:42 +0100

    feat: implemented customer get-, post- and put-request functionality fix: fixed REST naming conventions in CategoryController

--- src/main/java/nl/groep14/ipsen2BE/Controllers/CustomerController.java
+++ src/main/java/nl/groep14/ipsen2BE/Controllers/CustomerController.java
@@ -1,4 +1,57 @@
 package nl.groep14.ipsen2BE.Controllers;
 
+import nl.groep14.ipsen2BE.DAO.CategoryDAO;
+import nl.groep14.ipsen2BE.DAO.CustomerDAO;
+import nl.groep14.ipsen2BE.Models.ApiResponse;
+import nl.groep14.ipsen2BE.Models.Category;
+import nl.groep14.ipsen2BE.Models.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "/api/customers")
 public class CustomerController {
+    private final CustomerDAO customerDAO;
+
+    public CustomerController(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    @ResponseBody
+    public ApiResponse updateCustomer(@RequestBody Customer customer) {
+        if(this.customerDAO.getCustomerByID(customer.getId()).isEmpty()){
+            return new ApiResponse<>(HttpStatus.NOT_FOUND, "customer does not exist!");
+        }
+
+        this.customerDAO.saveToDatabase(customer);
+        return new ApiResponse<>(HttpStatus.ACCEPTED, "customer updated");
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResponse addCustomer(@RequestBody Customer customer) {
+
+        this.customerDAO.saveToDatabase(customer);
+        return new ApiResponse<>(HttpStatus.ACCEPTED, "customer added");
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<Customer> customers(){
+        ArrayList<Customer> customers = this.customerDAO.getAll();
+        return customers;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Optional<Customer> getCustomerByID(@PathVariable long id){
+        Optional<Customer> customer = this.customerDAO.getCustomerByID(id);
+        return customer;
+    }
+
 }
--- src/main/java/nl/groep14/ipsen2BE/DAO/CustomerDAO.java
+++ src/main/java/nl/groep14/ipsen2BE/DAO/CustomerDAO.java
@@ -1,4 +1,29 @@
 package nl.groep14.ipsen2BE.DAO;
 
+import nl.groep14.ipsen2BE.Models.Category;
+import nl.groep14.ipsen2BE.Models.Customer;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Component
 public class CustomerDAO {
+    private final CustomerRepository customerRepository;
+
+    public CustomerDAO(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void saveToDatabase(Customer customer){
+        this.customerRepository.save(customer);
+    }
+
+    public ArrayList<Customer> getAll(){
+        return (ArrayList<Customer>) this.customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerByID(Long id){
+        return this.customerRepository.findById(id);
+    }
 }
