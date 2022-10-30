10ba505ff6a540ebae59f6f4e9db602f23c1cdf9
TOGX <s1136032@student.hsleiden.nl>
2022-10-30 14:23:03 +0100

    FEAT: RecordDAO removedatabase functie toegevoegd

--- src/main/java/nl/groep14/ipsen2BE/DAO/RecordDAO.java
+++ src/main/java/nl/groep14/ipsen2BE/DAO/RecordDAO.java
@@ -1,4 +1,19 @@
 package nl.groep14.ipsen2BE.DAO;
 
+import org.springframework.stereotype.Component;
+import nl.groep14.ipsen2BE.Models.Record;
+
+@Component
 public class RecordDAO {
+
+    private final RecordRepository recordRepository;
+
+    public RecordDAO(RecordRepository recordRepository) {
+        this.recordRepository = recordRepository;
+    }
+
+    public void removeFromDatabase(Record record) {
+        this.recordRepository.delete(record);
+    }
+
 }
