6605d3a2bacdf91bf3a01f8a6125399b65f0418c
TOGX <s1136032@student.hsleiden.nl>
2022-10-30 14:37:10 +0100

    FIX: fixed exception not working

--- src/main/java/nl/groep14/ipsen2BE/Controllers/RecordController.java
+++ src/main/java/nl/groep14/ipsen2BE/Controllers/RecordController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import nl.groep14.ipsen2BE.Exceptions.RecordNotFoundException;
 
 import java.util.ArrayList;
 
--- src/main/java/nl/groep14/ipsen2BE/Models/ApiResponse.java
+++ src/main/java/nl/groep14/ipsen2BE/Models/ApiResponse.java
@@ -1,4 +1,32 @@
 package nl.groep14.ipsen2BE.Models;
 
-public class ApiResponse {
+import org.springframework.http.HttpStatus;
+
+import java.lang.reflect.Type;
+
+public class ApiResponse<Type> {
+    private HttpStatus code;
+    private String message;
+
+
+    public ApiResponse(HttpStatus code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public HttpStatus getCode() {
+        return code;
+    }
+
+    public void setCode(HttpStatus code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
