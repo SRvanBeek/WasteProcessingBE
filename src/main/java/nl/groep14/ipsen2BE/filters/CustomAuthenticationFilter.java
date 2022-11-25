a2b6683331d1aceb89571baa088f38b1a98dd18e
SRvanBeek <0705stijnvb@gmail.com>
2022-11-25 14:42:02 +0100

    chore: removed reduntant string in JWT-token output

--- src/main/java/nl/groep14/ipsen2BE/filters/CustomAuthenticationFilter.java
+++ src/main/java/nl/groep14/ipsen2BE/filters/CustomAuthenticationFilter.java
@@ -72,6 +72,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", accessToken);
         response.setContentType(APPLICATION_JSON_VALUE);
-        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+        new ObjectMapper().writeValue(response.getOutputStream(), accessToken);
     }
 }
